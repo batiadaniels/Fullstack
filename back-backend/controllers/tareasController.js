f22d02ddb1aaa7ce7fa69525e47fdadc5e36c39d
Batia Daniels <batiadaniels123@gmail.com>
2024-03-28 22:05:05 -0600

    Modelo creado y con insert y find de Tareas

--- back-backend/controllers/tareasController.js
+++ back-backend/controllers/tareasController.js
@@ -1,18 +1,36 @@
-const getTareas = (req, res) => {
-    res.status(200).json({ message: 'Obtener tareas'})
-}
+const asyncHandler = require('express-async-handler')
+const Tarea = require('../models/tareaModel')
 
-const postTareas = (req, res) => {
-    res.status(201).json({ message: 'Tarea Creada'})
-}
+const getTareas = asyncHandler( async (req, res) => {
 
-const  putTareas = (req, res) =>{
+    const tareas  = await Tarea.find()
+    res.status(200).json(tareas)
+})
+
+const postTareas = asyncHandler( async (req, res) => {
+    if(!req.body.texto){
+        res.status(400)
+        throw new Error('No escribiste una descripcion');
+    }
+    
+
+    const tarea = await Tarea.create({
+        texto: req.body.texto
+    })
+
+
+    res.status(201).json(tarea)
+})
+
+const  putTareas = asyncHandler ( async (req, res) =>{
     res.status(200).json({ message: `Se modifico la tarea ${req.params.id}`})
-}
+})
 
-const  deleteTareas = (req, res) =>{
+const  deleteTareas = asyncHandler( async (req, res) =>{
     res.status(200).json({ message: `Se elimino la tarea ${req.params.id}`})
-}
+})
+
+
 module.exports = {
     getTareas,
     postTareas,
